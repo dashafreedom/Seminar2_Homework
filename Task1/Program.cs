ba2982af9b59441b9bc14c0918974e61c52f0522
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-20 20:11:00 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, принимающая на вход трехзначное число и на выходе показывающая вторую цифру этого числа.
+
+Console.WriteLine("Ведите трёхзначное число: ");
+int threeDigitN = Convert.ToInt32(Console.ReadLine());
+
+int twoDigitN = threeDigitN / 10;
+int secondDigit = twoDigitN % 10;
+
+if(threeDigitN < 99 || threeDigitN > 1000)
+{
+    Console.WriteLine("Вы ввели некорректное число!");
+}
+else 
+{
+    Console.WriteLine(secondDigit);
+}
+
+
+
+
+
