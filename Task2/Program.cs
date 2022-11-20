46cfc6a20c2a05c6770befbada7d98edebb0aeab
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-20 22:51:13 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры не существует
+
+Console.Write("Введите число: ");
+string N = Convert.ToString(Console.ReadLine());
+
+if (N.Length > 2)
+{
+  Console.WriteLine(N[2]);
+}
+else
+ {
+  Console.WriteLine("третьей цифры не существует");
+}
